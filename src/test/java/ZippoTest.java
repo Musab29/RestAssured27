b1f30e9090624af8d0d8f6626a3858308c64d65a
pc <muhammedmermer2004@gmail.com>
2023-10-25 21:58:00 +0300

    bodyArrayHasSize

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -7,47 +7,50 @@ import static org.hamcrest.Matchers.*;
 public class ZippoTest {
 
     @Test
-    public void test1() {
+    public void test1(){
 
         given()
-
+                // Hazırlık işlemleri kodları
                 .when()
+                // endpoint (url), metod u verip istek gönderiliyor
 
                 .then()
-
+        // assertion, test, data işlemleri
         ;
     }
 
     @Test
-    public void statusCodeTest() {
+    public void statusCodeTest(){
+
         given()
-                // hazırlık
-                .when()
-                .get("http://api.zippopotam.us/us/90210")
-
-                .then()
-                .log().body() // dönen bady json data , log().all() : gidip gelen her şey
-                .statusCode(200) // test kısmı olduğundan assertion status code 200 mü
-
-        ;
-    }
-
-    @Test
-    public void contentTypeTest() {
-        given()
-
+                // hazırlık kısmı boş
                 .when()
                 .get("http://api.zippopotam.us/us/90210")
 
                 .then()
-                .log().body() // dönen bady json data , log().all() : gidip gelen her şey
+                .log().body() // dönen body json data , log().all() : gidip gelen her şey
                 .statusCode(200) // test kısmı olduğundan assertion status code 200 mü
-                .contentType(ContentType.JSON) // dönen datanın tipi JSON mı
         ;
     }
 
     @Test
-    public void checkCountryInResponseBody() {
+    public void contentTypeTest(){
+
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body() // dönen body json data , log().all() : gidip gelen her şey
+                .statusCode(200) // test kısmı olduğundan assertion status code 200 mü
+                .contentType(ContentType.JSON)  // dönen datanın tipi JSON mı
+        ;
+    }
+
+    @Test
+    public void checkCountryInResponseBody(){
+
         given()
 
                 .when()
@@ -55,17 +58,18 @@ public class ZippoTest {
 
                 .then()
                 //.log().body()
-                .statusCode(200)
-                .body("country", equalTo("United States")) // assertion
+                .statusCode(200)  // assertion
+                .body("country", equalTo("United States")) //assertion
+        // body nin country değişkeni "United States" eşit Mİ
         ;
     }
 
 
-    // Soru : "http://api.zippopotam.us/us/90210"  endpoint indne dönen
-    // place dizisinin ilk elemanının state değerinin  "California"
-    // olduğunu doğrulayınız
     @Test
-    public void checkstateInResponseBody() {
+    public void checkStateInResponseBody(){
+        // Soru : "http://api.zippopotam.us/us/90210"  endpoint indne dönen
+        // place dizisinin ilk elemanının state değerinin  "California"
+        // olduğunu doğrulayınız
         given()
 
                 .when()
@@ -73,8 +77,54 @@ public class ZippoTest {
 
                 .then()
                 .statusCode(200)
-                .body("country", equalTo("United States"))
-                .body("places[0].state", equalTo("California"))
+                .body("places[0].state", equalTo("California"));
         ;
     }
+
+
+    @Test
+    public void checkHasItem(){
+        // Soru : "http://api.zippopotam.us/tr/01000"  endpoint in dönen
+        // place dizisinin herhangi bir elemanında  "Dörtağaç Köyü" değerinin
+        // olduğunu doğrulayınız
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/tr/01000")
+
+                .then()
+                //.log().body()
+                .body("places.'place name'", hasItem("Dörtağaç Köyü"))
+                .statusCode(200)
+        ;
+    }
+
+    // Soru : "http://api.zippopotam.us/us/90210"  endpoint in dönen
+    // place dizisinin dizi uzunluğunun 1 olduğunu doğrulayınız.
+
+    @Test
+    public void bodyArrayHasSizeTest(){
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .body("places", hasSize(1)); // places ın item size 1 e eşit mi
+        ;
+    }
+
+    @Test
+    public void bodyArrayHasSizeTest2(){
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .body("places.size()", equalTo(21)); // places ın item size 1 e eşit mi
+        ;
+    }
+
+
 }
